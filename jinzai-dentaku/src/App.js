106e76eeea9e64252182e00891a18fbc5c5979e0
Zaneo <gareth.higgins@ryerson.ca>
2022-03-16 11:07:22 +0900

    Change text to test github action

--- jinzai-dentaku/src/App.js
+++ jinzai-dentaku/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <p1>Testing Github Pages with react and github actions</p1>
         </p>
         <a
           className="App-link"
